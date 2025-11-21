0e9c66f9264c7235e8d257562613e8e3d96ac99a
YusufCaesareaArianto <230712331@students.uajy.ac.id>
2025-11-21 12:12:22 +0700

    LOGIN
    Buat LOGIN

--- Travel_A_7/src/main.jsx
+++ Travel_A_7/src/main.jsx
@@ -3,13 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
-<<<<<<< HEAD
 import Login from "./pages/login";
 import Register from "./pages/Register";
-=======
-import Register from "./pages/register.jsx";
->>>>>>> f7fbd116a4c698787bc06ef63fe81988b5a69a96
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -18,10 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
+
         <Route path="/home" element={<Login />} />
-=======
->>>>>>> f7fbd116a4c698787bc06ef63fe81988b5a69a96
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
